b89ea3b79bcb97ea261c2966e4f5ac31ccb78d68
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-04 22:49:09 +0900

    check the boxes conditionally

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,7 @@ import React from "react";
 const ToDo = (props) => {
   return (
     <div className="todo-item">
-      <input type="checkbox"></input>
+      <input type="checkbox" checked={props.values.completed}></input>
       <p>{props.values.text}</p>
     </div>
   );
