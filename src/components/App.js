9f51c0e6daf5c41c14185ba58b4de7e6fb830b0d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-05 13:55:16 +0900

    using handleChange to pass state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDo from "./ToDo";
 import todosData from "../taskData";
 
 function App() {
+  // const [clicked, setClicked] = useState()
+
+  const handleChange = (id) => {
+    console.log(`Clicked ${id}`);
+  };
+
   const todoData = todosData.map((element) => (
-    <ToDo key={element.id} values={element} />
+    <ToDo key={element.id} values={element} handleChange={handleChange} />
   ));
   return <div className="todo-list">{todoData}</div>;
 }
