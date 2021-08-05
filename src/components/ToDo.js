efcff8d4f385187d882ed2b0a5dd8430a7dd866c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-05 14:00:31 +0900

    passed the handler down to the childer component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const ToDo = (props) => {
+  console.log(props);
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.values.completed}></input>
+      <input
+        type="checkbox"
+        checked={props.values.completed}
+        onChange={() => {
+          props.handleChange(props.values.id);
+        }}
+      ></input>
       <p>{props.values.text}</p>
     </div>
   );
